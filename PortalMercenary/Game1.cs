7cacc5001a2ae0e62a03c1ad836cef164254e9a8
gandifil <gandifilmk@gmail.com>
2026-05-11 07:35:17 +0300

    Add background

--- PortalMercenary/Game1.cs
+++ PortalMercenary/Game1.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
 using MonoGame.Extended.Graphics;
+using MonoGame.Extended.Particles.Modifiers.Interpolators;
 
 namespace PortalMercenary;
 
 public class Game1 : Game
 {
-    private Texture2DAtlas _atlas;
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
@@ -17,16 +18,26 @@ public class Game1 : Game
     // The offset to apply when drawing the background pattern so it appears to
     // be scrolling.
     private Vector2 _backgroundOffset;
+    private Texture2D _backgroundPattern;
+    private Texture2DAtlas _atlas;
+    private Texture2D _atlasTexture2D;
 
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
+        _graphics.PreferredBackBufferWidth = 800;
+        _graphics.PreferredBackBufferHeight = 450;
+        _graphics.ApplyChanges();
         Content.RootDirectory = "Content";
+        Window.Title = "Portal Mercenary";
+        Window.AllowUserResizing = true;
         IsMouseVisible = true;
     }
 
     protected override void Initialize()
     {
+        // Create a new graphics device manager.
+
         // Initialize the offset of the background pattern at zero.
         _backgroundOffset = Vector2.Zero;
 
@@ -41,9 +52,11 @@ public class Game1 : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        var cardsTexture = Content.Load<Texture2D>("images/Tilemap_color3");
-        _atlas = Texture2DAtlas.Create("images/Tilemap_color3", cardsTexture, 62, 64);
-        // TODO: use this.Content to load your game content here
+        _atlasTexture2D = Content.Load<Texture2D>("images/Tilemap_color3");
+        _atlas = Texture2DAtlas.Create("images/Tilemap_color3", _atlasTexture2D, 64, 64);
+
+        // Load the background pattern texture.
+        _backgroundPattern = Content.Load<Texture2D>("images/grass_tile");
     }
 
     protected override void Update(GameTime gameTime)
@@ -59,12 +72,11 @@ public class Game1 : Game
 
     protected override void Draw(GameTime gameTime)
     {
+        GraphicsDevice.Clear(new Color(32, 40, 78, 255));
+
         _spriteBatch.Begin(samplerState: SamplerState.PointWrap);
-
-        _spriteBatch.Draw(_atlas[15], new Rectangle(_backgroundOffset.ToPoint(), _backgroundDestination.Size), Color.White);
-
+        _spriteBatch.Draw(_backgroundPattern, GraphicsDevice.PresentationParameters.Bounds, new Rectangle(_backgroundOffset.ToPoint(), GraphicsDevice.PresentationParameters.Bounds.Size), Color.White);
         _spriteBatch.End();
-
 
         base.Draw(gameTime);
     }
