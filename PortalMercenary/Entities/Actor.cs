56ec5586cb4fe762b95d3bf2b108b805d8d08d1c
gandifil <gandifilmk@gmail.com>
2026-07-11 22:34:21 +0300

    Add body class

--- PortalMercenary/Entities/Actor.cs
+++ PortalMercenary/Entities/Actor.cs
@@ -4,60 +4,33 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended;
 using MonoGame.Extended.Graphics;
-using MonoGame.Extended.Tilemaps.Tiled.Document;
 
 namespace PortalMercenary.Entities;
 
 public class Actor
 {
-    private readonly ActorPart[] _parts, _legs;
-    private readonly ActorPart _head, _body, _weapon;
+    private readonly ActorBody _body;
 
     public Vector2 Position { get; set; } = Vector2.Zero;
 
     public ActorOptions Options { get; set; } = new();
 
     public Direction Direction { get; set; }
+    public Vector2 Shift { get; set; }
 
     private float _t; 
     
     public Actor(Texture2DAtlas textureAtlas)
     {
-        _parts = new ActorPart[5];
-        _legs = new ActorPart[2];
-
-        var bootSprites = AtlasParser.GetBootSprites(textureAtlas);
-        _parts[0] = _legs[0] = new ActorPart(this, bootSprites)
-        {
-            Shift = new  Vector2(-10, 0),
-        };
-        
-        _parts[1] = _legs[1] = new ActorPart(this, bootSprites)
-        {
-            Shift = new  Vector2(10, 0)
-        };
-
-        _parts[2] = _body = new ActorPart(this, AtlasParser.GetBodySprites(textureAtlas))
-        {
-            Shift = new  Vector2(0, -20f),
-        };
-
-        _parts[3] = _head = new ActorPart(this, AtlasParser.GetHeadSprites(textureAtlas))
-        {
-            Shift = new  Vector2(0, -70f),
-        };
-
-        _parts[4] = _weapon = new ActorPart(this, AtlasParser.GetWeaponSprites(textureAtlas))
-        {
-            Shift = new  Vector2(0, -20f),
-        };
+        _body = new ActorBody(this, textureAtlas);
     }
     
     public void Update(GameTime gameTime, Vector2 shift)
     {
         if (shift == Vector2.Zero)
             return;
-        
+
+        Shift = shift;
         if (shift.Y > 0)
             Direction =  Direction.Down;
         else if (shift.Y < 0)
@@ -67,36 +40,35 @@ public class Actor
         else if (shift.X < 0)
             Direction =  Direction.Left;
         
-        Position += shift;
+        Position += Shift;
         
         var dt = (float)gameTime.ElapsedGameTime.TotalSeconds * Options.Speed;
         _t += shift.X > 0 ? dt: -dt; // по или против часовой стрелки
         var pos = new Vector2(Options.StepHeight * MathF.Cos(_t), Options.StepWidth * MathF.Sin(_t));
 
         pos.Rotate(shift.ToAngle());
-        _legs[0].Position = pos;
-        _legs[1].Position = -pos;
-        //_head.Depth = -0.4f;
-        _body.Position = new Vector2(0, Options.StepHeight * (float)Math.Sin(_t));
-        SetWeaponPosition(shift, _t * 0.66f);
+        _body.Legs[0].Position = pos;
+        _body.Legs[1].Position = -pos;
+        _body.Body.Position = new Vector2(0, Options.StepHeight * (float)Math.Sin(_t));
+        SetWeaponPosition(_t * 0.66f);
     }
 
-    private void SetWeaponPosition(Vector2 shift, float d)
+    private void SetWeaponPosition(float d)
     {
         var pos = new Vector2(20f, 0);
-        var radians = shift.ToAngle();
+        var radians = Shift.ToAngle();
         pos.RotateAround(Vector2.Zero, radians);
         var fluctuating = 10f * new Vector2(3 * MathF.Cos(d), MathF.Abs(MathF.Sin(d)));
         fluctuating.Rotate(radians - MathF.PI / 2);
-        _weapon.Position = pos + fluctuating;
-        _weapon.Depth = pos.Y / 100f;
-        _weapon.Rotation = radians - MathF.PI / 2;
+        _body.Weapon.Position = pos + fluctuating;
+        _body.Weapon.Depth = pos.Y / 100f;
+        _body.Weapon.Rotation = radians - MathF.PI / 2;
     }
 
     public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
     {
         spriteBatch.DrawCircle(Position, 5, 16, Color.Red);
-        foreach (var part in _parts.OrderBy(x => x.Depth))
+        foreach (var part in _body.Parts.OrderBy(x => x.Depth))
             part.Draw(spriteBatch);
     }
 }
---
+++ PortalMercenary/Entities/ActorBody.cs
@@ -0,0 +1,42 @@
+using Microsoft.Xna.Framework;
+using MonoGame.Extended.Graphics;
+
+namespace PortalMercenary.Entities;
+
+public class ActorBody
+{
+    public readonly ActorPart[] Parts, Legs;
+    public readonly ActorPart Head, Body, Weapon;
+    
+    public ActorBody(Actor actor, Texture2DAtlas textureAtlas)
+    {
+        Parts = new ActorPart[5];
+        Legs = new ActorPart[2];
+
+        var bootSprites = AtlasParser.GetBootSprites(textureAtlas);
+        Parts[0] = Legs[0] = new ActorPart(actor, bootSprites)
+        {
+            Shift = new  Vector2(-10, 0),
+        };
+        
+        Parts[1] = Legs[1] = new ActorPart(actor, bootSprites)
+        {
+            Shift = new  Vector2(10, 0)
+        };
+
+        Parts[2] = Body = new ActorPart(actor, AtlasParser.GetBodySprites(textureAtlas))
+        {
+            Shift = new  Vector2(0, -20f),
+        };
+
+        Parts[3] = Head = new ActorPart(actor, AtlasParser.GetHeadSprites(textureAtlas))
+        {
+            Shift = new  Vector2(0, -70f),
+        };
+
+        Parts[4] = Weapon = new ActorPart(actor, AtlasParser.GetWeaponSprites(textureAtlas))
+        {
+            Shift = new  Vector2(0, -20f),
+        };
+    }
+}
